d32722622645581b6521293c9726ce545a19882b
Karol2401 <karol.m2401@gmail.com>
2022-12-29 18:46:08 +0000

    Add base HTML structure and dociment objects to javascript

---
+++ assets/js/script.js
@@ -0,0 +1,3 @@
+const playerSelection = document.getElementById('player-selection')
+const computerSelection = document.getElementById('computer-selection')
+const resultShown = document.getElementById('result')
