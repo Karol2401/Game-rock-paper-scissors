1a5a4fa9ef80585ffe8daa8571b5f38452c07275
Karol2401 <karol.m2401@gmail.com>
2022-12-29 18:59:28 +0000

    Add functions and querySelectorAll

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,13 @@
 const playerSelection = document.getElementById('player-selection')
 const computerSelection = document.getElementById('computer-selection')
-const resultShown = document.getElementById('result')
+const resultDisplay = document.getElementById('result')
+const possibleSelections = document.querySelectorAll('button')
+
+function generateComputerSelection () {
+
+}
+
+function getResult () {
+
+}
+
