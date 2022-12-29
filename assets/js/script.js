230e8de8866ef29b9394f3e5611eb67a82d81995
Karol2401 <karol.m2401@gmail.com>
2022-12-29 20:22:11 +0000

    Add click event and creating a number generator for the computer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,38 @@
-const playerSelection = document.getElementById('player-selection')
-const computerSelection = document.getElementById('computer-selection')
+// Get access to HTML elements
+const playerSelectionDisplay = document.getElementById('player-selection')
+const computerSelectionDisplay = document.getElementById('computer-selection')
 const resultDisplay = document.getElementById('result')
+
+// Creation of an array of all buttons
 const possibleSelections = document.querySelectorAll('button')
 
-function generateComputerSelection () {
+// Get access to playerSelection and computerSelection from anywere
+let playerSelection
+let computerSelection
 
+//Add event when clicking one of the 3 buttons
+possibleSelections.forEach(button => button.addEventListener('click', (event) => {
+    playerSelection = event.target.id
+    playerSelectionDisplay.innerHTML = playerSelection
+    generateComputerSelection ()
+}))
+
+//Generate a random number from 0 to 2 and assign it to the elements rock paper scissors
+function generateComputerSelection () {
+    const randomNumber = Math.floor(Math.random() * 3)
+
+    if (randomNumber === 0) {
+        computerSelection = 'scissors'
+    }
+
+    if (randomNumber === 1) {
+        computerSelection = 'paper'
+    }
+
+    if (randomNumber === 2) {
+        computerSelection = 'rock'
+    }
+    computerSelectionDisplay.innerHTML = computerSelection
 }
 
 function getResult () {
