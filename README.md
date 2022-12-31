e0583a48331000f7e2023421afa5980a3a4d3f42
Karol2401 <karol.m2401@gmail.com>
2022-12-31 18:31:39 +0000

    Add semicolons

---
+++ README.md
@@ -0,0 +1,50 @@
+# Rock Paper Scissors #
+
+ Rock Paper Scissors is a simple game that demonstrates how JavaSricpt works. The game uses a random mechanism to generate the computer's choices. As a user, we can compete against the computer and see if we can win.
+
+ ## Features ##
+
+ ### Existing Features ###
+ 
+ + Rock Paper Scissors Heading
+ 
+  * Featured at the top of the page, the Rock Paper Scissors heading is easy to see for the user. Upon viewing the page, the user will be able to see the name of the game.
+
+![Page title](/assets/images/Screenshot%202022-12-31%20at%2017.35.25.png)
+
++ The Game Area
+  * In this section, the user will be able to play the game of paper and scissors. The user will be able to easily see the icons for the games stone, paper, scissors.
+  * The user will be able to choose the type of object to play in the next round.
+
+ ![Buttons image](/assets/images/Screenshot%202022-12-31%20at%2017.48.28.png)
+
+ + The Score area
+   * In this section the user will be able to check their selection and the computer selection, who won the round and the current score.
+
+  ![Table of results](/assets/images/Screenshot%202022-12-31%20at%2017.54.11.png)
+
+  + End game area
+    * When a user or computer reaches 5 points, the site will automatically take you to a page with one of two  a refresh link.
+
+  ![You are lose. Restart link](/assets/images/Screenshot%202022-12-31%20at%2017.58.43.png)
+  ![You are win. Restart link](/assets/images/Screenshot%202022-12-31%20at%2017.59.01.png)
+
+  + Rules of the game area
+    * At the bottom of the page user will find a description of the game and the rules.
+
+  ![Rules of the game](/assets/images/Screenshot%202022-12-31%20at%2018.03.37.png)
+
+  ## Testing 
+
+  * I tested that this site works in different browsers: Chrome, Safari, Firefox, Opera.
+  * I confirmed that this site is responsive, looks good and functions well on all standard screen sizes using the Devtools device toolbar.
+  * I confirmed that the Navigation, Header and Rules text are all readable and easy to understand.
+  * I confirm that all buttons are working correctly and the scoreboard is always updating.
+  * I confirm the links in the page all works.
+
+  ## Validator Testing
+  + HTML
+    * No errors were returned when passing through the official W3C validator.
+  + CSS
+    *  No errors were found when passing through the official (Jigsaw) validator
+  +
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,101 +1,100 @@
 // Get access to HTML elements
-const startPage = document.getElementById('start')
-const gameOverPage = document.getElementById('game-over')
-const playerSelectionDisplay = document.getElementById('player-selection')
-const computerSelectionDisplay = document.getElementById('computer-selection')
-const resultDisplay = document.getElementById('result')
-const playerScoreDisplay = document.getElementById('player-score')
-const computerScoreDisplay = document.getElementById('computer-score')
+const startPage = document.getElementById('start');
+const gameOverPage = document.getElementById('game-over');
+const playerSelectionDisplay = document.getElementById('player-selection');
+const computerSelectionDisplay = document.getElementById('computer-selection');
+const resultDisplay = document.getElementById('result');
+const playerScoreDisplay = document.getElementById('player-score');
+const computerScoreDisplay = document.getElementById('computer-score');
 
 // Creation of an array of all buttons
-const possibleSelections = document.querySelectorAll('button')
+const possibleSelections = document.querySelectorAll('button');
 
 // Get access to playerSelection, computerSelection, playerScore, computerScore, result and score from anywere
-let playerSelection
-let computerSelection
-let result
-let finalResult
-let playerScore = 0
-let computerScore = 0
+let playerSelection;
+let computerSelection;
+let result;
+let finalResult;
+let playerScore = 0;
+let computerScore = 0;
 
 //Add event when clicking one of the 3 buttons
 possibleSelections.forEach(button => button.addEventListener('click', (e) => {
-    playerSelection = e.target.id
-    playerSelectionDisplay.innerHTML = playerSelection
-    generateComputerSelection ()
-    getResult()
-    gameOver ()
-}))
+    playerSelection = e.target.id;
+    playerSelectionDisplay.innerHTML = playerSelection;
+    generateComputerSelection ();
+    getResult();
+    gameOver ();
+}));
 
 //Generate a random number from 0 to 2 and assign it to the elements rock paper scissors
 function generateComputerSelection () {
-    const randomNumber = Math.floor(Math.random() * 3)
+    const randomNumber = Math.floor(Math.random() * 3);
 
     if (randomNumber === 0) {
-        computerSelection = '✂️'
+        computerSelection = '✂️';
     }
 
     if (randomNumber === 1) {
-        computerSelection = '📃'
+        computerSelection = '📃';
     }
 
     if (randomNumber === 2) {
-        computerSelection = '🗿'
+        computerSelection = '🗿';
     }
-    computerSelectionDisplay.innerHTML = computerSelection
+    computerSelectionDisplay.innerHTML = computerSelection;
 }
 
 //Comparison of computer and player choices, matching of results and add score
 function getResult () {
     if (computerSelection === playerSelection) {
-        result = "Draw"    
+        result = "Draw"   ; 
     }
     if (computerSelection === '🗿' && playerSelection === '✂️') {
-        result = "You lose"
-        computerScore += 1
-        computerScoreDisplay.innerText = computerScore
+        result = "You lose";
+        computerScore += 1;
+        computerScoreDisplay.innerText = computerScore;
 
     }
     if (computerSelection === '✂️' && playerSelection === '📃') {
-        result = "You lose"
-        computerScore += 1
-        computerScoreDisplay.innerText = computerScore
+        result = "You lose";
+        computerScore += 1;
+        computerScoreDisplay.innerText = computerScore;
     }
     if (computerSelection === '📃' && playerSelection === '🗿') {
-        result = "You lose"
-        computerScore += 1
-        computerScoreDisplay.innerText = computerScore
+        result = "You lose";
+        computerScore += 1;
+        computerScoreDisplay.innerText = computerScore;
     }
     if (computerSelection === '✂️' && playerSelection === '🗿') {
-        result = "You win"
-        playerScore += 1
-        playerScoreDisplay.innerText = playerScore
+        result = "You win";
+        playerScore += 1;
+        playerScoreDisplay.innerText = playerScore;
     }
     if (computerSelection === '📃' && playerSelection === '✂️') {
-        result = "You win"
-        playerScore += 1
-        playerScoreDisplay.innerText = playerScore
+        result = "You win";
+        playerScore += 1;
+        playerScoreDisplay.innerText = playerScore;
     }
     if (computerSelection === '🗿' && playerSelection === '📃') {
-        result = "You win"
-        playerScore += 1
-        playerScoreDisplay.innerText = playerScore
+        result = "You win";
+        playerScore += 1;
+        playerScoreDisplay.innerText = playerScore;
     }
-    resultDisplay.innerHTML = result
+    resultDisplay.innerHTML = result;
 }
 
 //Window with end-of-game information and button to restart the game
 function gameOver () {
     if (playerScore == 5) {
-        startPage.style.display = 'none'
-        gameOverPage.style.display = 'grid'
-        finalResult = '<a href="https://karol2401.github.io/Game-rock-paper-scissors/">You are win. Try again!</a>'
+        startPage.style.display = 'none';
+        gameOverPage.style.display = 'grid';
+        finalResult = '<a href="https://karol2401.github.io/Game-rock-paper-scissors/">You are win. Try again!</a>';
     }
     if (computerScore == 5) {
-        startPage.style.display = 'none'
-        gameOverPage.style.display = 'grid' 
-        finalResult = '<a href="https://karol2401.github.io/Game-rock-paper-scissors/">You are lose. Try again!</a>'  
+        startPage.style.display = 'none';
+        gameOverPage.style.display = 'grid'; 
+        finalResult = '<a href="https://karol2401.github.io/Game-rock-paper-scissors/">You are lose. Try again!</a>'; 
     }
-    gameOverPage.innerHTML = finalResult
+    gameOverPage.innerHTML = finalResult;
 }
-
